6f23f3169d4d73eee378b62be532fc0d37d0137b
DURGESH JADHAV <97460307+Decode749@users.noreply.github.com>
2023-03-28 11:10:10 +0530

    babbar way to do sum of array elements done

--- 17.RECURSION/06.sum_of_array_elemets.cpp
+++ 17.RECURSION/06.sum_of_array_elemets.cpp
@@ -1,22 +1,45 @@
 #include<iostream>
 using namespace std;
 
-int Sum(int *arr , int size , int ans)
+// int Sum(int *arr , int size , int ans)
+// {
+//     if(size == 0)
+//     {
+//         return ans;
+//     }
+//     ans += arr[0];
+
+//     return Sum(arr + 1 , size - 1 , ans);
+
+// }
+// int main()
+// {
+//     int arr[5] = { 10 , 20 , 30 , 40 , 50};
+//     int size = 5;
+//     int ans = 0;
+//     cout<<Sum(arr,size,ans);
+    
+// }
+
+// by babbar way 
+
+int getSum(int arr[] , int size)
 {
     if(size == 0)
     {
-        return ans;
+        return 0;
     }
-    ans += arr[0];
-
-    return Sum(arr + 1 , size - 1 , ans);
-
+    if(size == 1)
+    {
+        return arr[0];
+    }
+    int sum = arr[0] + getSum(arr + 1 , size - 1);
+    return sum;
 }
+
 int main()
 {
-    int arr[5] = { 10 , 20 , 30 , 40 , 50};
+    int arr[5] = {1 , 2 , 3 , 4 , 5};
     int size = 5;
-    int ans = 0;
-    cout<<Sum(arr,size,ans);
-    
+    cout<<getSum(arr,size);
 }
